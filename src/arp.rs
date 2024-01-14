11182dea36e3415323ef94a9e26e891f5b85a7c6
sat0ken <15720506+sat0ken@users.noreply.github.com>
2024-01-14 10:52:13 +0900

    arpの実装を追加

--- src/arp.rs
+++ src/arp.rs
@@ -1,9 +1,29 @@
 use std::sync::Mutex;
+use bytes::{Buf, BufMut};
+use crate::ethernet::ETHERNET_TYPE_IPV4;
+use crate::util::{to_u32};
 
 #[derive(Debug)]
 pub struct ArpTable {
     mac_addr: [u8; 6],
     ip_addr: u32
+}
+
+const ARP_HARDWARE_TYPE: u16 = 0x0001;
+const ARP_OPERATION_TYPE_REQUEST: u16 = 0x0001;
+const ARP_OPERATION_TYPE_REPLY: u16 = 0x0002;
+
+#[derive(Debug)]
+struct ArpMessage {
+    hardware_type: u16,
+    protocol_type: u16,
+    hardware_addr_len: u8,
+    protocol_addr_len: u8,
+    operation_type: u16,
+    src_mac_addr: [u8; 6],
+    src_ip_addr: u32,
+    dst_mac_addr: [u8; 6],
+    dst_ip_addr: u32,
 }
 
 static ARP_TABLES: Mutex<Vec<ArpTable>> = Mutex::new(Vec::new());
@@ -23,4 +43,58 @@ pub fn add_arp_tables(mac_addr: [u8; 6], ip_addr: u32) {
     let mut arp = ARP_TABLES.lock().unwrap();
     arp.push(ArpTable{mac_addr, ip_addr});
     println!("add arp tables entry is OK")
+}
+
+pub fn read_arp_packet(packet: Vec<u8>, my_mac_addr: [u8; 6], my_ip_addr: u32) -> (u32, Vec<u8>) {
+
+    let mut arp = &packet[..];
+    let arp_message = ArpMessage{
+        hardware_type: arp.get_u16(),
+        protocol_type: arp.get_u16(),
+        hardware_addr_len: arp.get_u8(),
+        protocol_addr_len: arp.get_u8(),
+        operation_type: arp.get_u16(),
+        // let src_mac_addr = <[u8; 6]>::try_from(arp.get_uint(6).to_be_bytes().get(2..).unwrap()).unwrap();
+        src_mac_addr: arp[0..6].try_into().unwrap(),
+        src_ip_addr: to_u32(&arp[6..10]),
+        dst_mac_addr: arp[10..16].try_into().unwrap(),
+        dst_ip_addr: to_u32(&arp[16..20]),
+    };
+
+    // ARPテーブルを検索して存在していなければ追加
+    if search_arp_tables(arp_message.src_ip_addr) == [0, 0, 0, 0, 0, 0] {
+        add_arp_tables(arp_message.src_mac_addr, arp_message.src_ip_addr)
+    }
+
+    if arp_message.operation_type == ARP_OPERATION_TYPE_REQUEST &&
+        arp_message.dst_ip_addr == my_ip_addr {
+        return (arp_message.src_ip_addr, out_arp_reply(arp_message, my_mac_addr, my_ip_addr));
+    }
+    return (0, vec![]);
+}
+
+fn out_arp_reply(arp_req: ArpMessage, my_mac_addr: [u8; 6], my_ip_addr: u32) -> Vec<u8> {
+    let reply = ArpMessage{
+        hardware_type: ARP_HARDWARE_TYPE,
+        protocol_type: ETHERNET_TYPE_IPV4,
+        hardware_addr_len: 6,
+        protocol_addr_len: 4,
+        operation_type: ARP_OPERATION_TYPE_REPLY,
+        src_mac_addr: my_mac_addr,
+        src_ip_addr: my_ip_addr,
+        dst_mac_addr: arp_req.src_mac_addr,
+        dst_ip_addr: arp_req.src_ip_addr,
+    };
+    let mut buf = Vec::new();
+    buf.put_u16(reply.hardware_type);
+    buf.put_u16(reply.protocol_type);
+    buf.put_u8(reply.hardware_addr_len);
+    buf.put_u8(reply.protocol_addr_len);
+    buf.put_u16(reply.operation_type);
+    buf.append(&mut reply.src_mac_addr.to_vec());
+    buf.put_u32(reply.src_ip_addr);
+    buf.append(&mut reply.dst_mac_addr.to_vec());
+    buf.put_u32(reply.dst_ip_addr);
+
+    return buf;
 }
--- src/ethernet.rs
+++ src/ethernet.rs
@@ -1,43 +1,47 @@
 use std::sync::mpsc::{SyncSender};
 use bytes::BufMut;
-use crate::arp::search_arp_tables;
+use crate::arp::{read_arp_packet, search_arp_tables};
 use crate::ipv4::read_ipv4_packet;
-use crate::util::dump_packet;
+use crate::util::{to_u16};
 
-const ETHERNET_TYPE_IPV4: u16 = 0x0800;
+pub const ETHERNET_TYPE_IPV4: u16 = 0x0800;
 const ETHERNET_TYPE_ARP: u16  = 0x0806;
 
+// const ETHERNET_BRD_ADDR: [u8; 6] = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
+
 pub struct EthernetHeader {
-    pub dest_mac_addr: [u8; 6],
-    pub src_mac_addr: [u8; 6],
-    pub ethernet_type: u16
+    pub dst_mac_addr: [u8; 6], // 宛先MACアドレス
+    pub src_mac_addr: [u8; 6],  // 送信元MACアドレス
+    pub ethernet_type: u16      // Ethernetタイプ
 }
 
-pub fn read_ethernet(packet: Vec<u8>, tx: SyncSender<Vec<u8>>, my_mac_addr: [u8; 6], ipv4: u32) {
+pub fn read_ethernet(packet: Vec<u8>, tx: SyncSender<Vec<u8>>, my_mac_addr: [u8; 6], my_ip_addr: u32) {
 
     let eth_header = EthernetHeader{
-        dest_mac_addr:   packet[0..6].try_into().unwrap(),
-        src_mac_addr:    packet[6..12].try_into().unwrap(),
-        ethernet_type: ((packet[12] as u16) << 8) | packet[13] as u16
+        dst_mac_addr: packet[0..6].try_into().unwrap(),
+        src_mac_addr:  packet[6..12].try_into().unwrap(),
+        ethernet_type: to_u16(packet.get(12..14).unwrap())
     };
 
-
-    if my_mac_addr != eth_header.dest_mac_addr {
+    if my_mac_addr != eth_header.dst_mac_addr {
         tx.send(vec![]).unwrap();
     }
 
     match eth_header.ethernet_type {
         ETHERNET_TYPE_IPV4 => {
             println!("receive ipv4 packet");
-            dump_packet(packet[14..].to_owned());
-            let mac = eth_header.dest_mac_addr.clone();
-            let (dest_ip_addr ,packet) = read_ipv4_packet(eth_header, packet[14..].to_owned(), ipv4);
+            let mac = eth_header.dst_mac_addr;
+            let (dest_ip_addr, packet) = read_ipv4_packet(eth_header, packet[14..].to_owned(), my_ip_addr);
             if dest_ip_addr != 0 {
                 out_ethernet(tx, mac, dest_ip_addr, packet, ETHERNET_TYPE_IPV4);
             }
         }
         ETHERNET_TYPE_ARP => {
             println!("receive arp packet");
+            let (dest_ip_addr, packet) = read_arp_packet(packet[14..].to_owned(), my_mac_addr, my_ip_addr);
+            if dest_ip_addr != 0 {
+                out_ethernet(tx, my_mac_addr, dest_ip_addr, packet, ETHERNET_TYPE_ARP);
+            }
         }
         _ => {}
     }
