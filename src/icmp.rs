67bf97345c9314846783ecc146654a1e8d60d3b3
sat0ken <15720506+sat0ken@users.noreply.github.com>
2024-01-14 10:50:57 +0900

    パケットのセットをリファクタ

--- src/icmp.rs
+++ src/icmp.rs
@@ -1,26 +1,27 @@
-use crate::util::{to_u16};
-use bytes::BufMut;
+use bytes::{Buf, BufMut};
 
 const ICMP_MESSAGE_TYPE_ECHO_REPLY: u8 = 0;
 const ICMP_MESSAGE_TYPE_ECHO_REQUEST: u8 = 8;
 
 struct ICMPHeader {
-    icmp_type: u8,
+    icmp_type: u8,  // メッセージタイプ
     icmp_code: u8,
-    checksum: u16
+    checksum: u16   // チェックサム
 }
 
 struct ICMPEchoMessage<'a> {
-    identity_number: u16,
-    sequence_number: u16,
-    data: &'a [u8],
+    identity_number: u16,   // ID
+    sequence_number: u16,   // シーケンス番号
+    data: &'a [u8],         // データ
 }
 
 pub fn read_icmp_packet(icmp_packet: Vec<u8>) -> Vec<u8> {
+    let mut packet = &icmp_packet[..];
+
     let icmp_header = ICMPHeader{
-        icmp_type: icmp_packet[0],
-        icmp_code: icmp_packet[1],
-        checksum: to_u16(icmp_packet.get(2..4).unwrap()),
+        icmp_type: packet.get_u8(),
+        icmp_code: packet.get_u8(),
+        checksum:  packet.get_u16(),
     };
 
     match icmp_header.icmp_type {
@@ -28,9 +29,9 @@ pub fn read_icmp_packet(icmp_packet: Vec<u8>) -> Vec<u8> {
         ICMP_MESSAGE_TYPE_ECHO_REQUEST => {
             println!("icmp echo request");
             let echo = ICMPEchoMessage{
-                identity_number: to_u16(icmp_packet.get(4..6).unwrap()),
-                sequence_number: to_u16(icmp_packet.get(6..8).unwrap()),
-                data: icmp_packet.get(8..).unwrap()
+                identity_number: packet.get_u16(),
+                sequence_number: packet.get_u16(),
+                data: packet.get(0..).unwrap()
             };
             return icmp_echo_reply(icmp_header, echo);
         }
